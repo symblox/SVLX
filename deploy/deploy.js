554f365fd6d321f7c2b23592ffcef576a72328ba
Symblox Dev <symblox@protonmail.com>
2021-03-10 23:14:56 +0800

    chg: update deploy script

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -16,8 +16,20 @@ module.exports = async hardhat => {
 
     const { deploy } = deployments;
 
-    await deploy("SVLX", {
+    const svlxContract = await deploy("SVLX", {
         from: deployer,
         proxy: "initialize"
     });
+    debug("\n  svlxContract:  ", svlxContract.address);
+    // Add the default pool
+    const chainId = parseInt(await getChainId(), 10);
+    const STAKE_POOLS = {
+        106: "0x7f7697E82be5d7F41De6b283Ca562e4D79a4F74a",
+        111: "0x267Ec0079043B43930a1d671FB98fD19FdCaF449"
+    };
+    debug("\n  Add staking pool:  ", STAKE_POOLS[chainId]);
+    const svlx = await ethers.getContractAt("SVLX", svlxContract.address, signer);
+    await svlx.addPool(STAKE_POOLS[chainId]);
+
+    console.log(`Deployed SVLX ${svlxContract.address}\nAdded stake pool ${STAKE_POOLS[chainId]}`);
 };
